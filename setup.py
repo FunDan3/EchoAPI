740dc49638c2a3b3f2e4fcb4ec08f636f6ce9720
Fun_Dan3 <dfr34560@gmail.com>
2024-03-19 16:07:01 +0300

    Forgot to update the version AGAIN

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
 	name = "EchoMessagerAPI",
-	version = "0.0.7",
+	version = "0.0.8",
 	author = "Fun_Dan3",
 	author_email = "dfr34560@gmail.com",
 	description = "API to access EchoServer post-quantum messager.",
